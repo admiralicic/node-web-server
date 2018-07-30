d12fd1c32f11c565d56093acb13c3c8499a8a2a2
Admir Alicic <admir.alicic@gmail.com>
2018-07-30 23:48:29 +0200

    Add handlebars

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send({name: 'Admir', likes: [
-        'biking',
-        'cities'
-    ]});
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
